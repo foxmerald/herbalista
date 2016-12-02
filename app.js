70909e2702b8204634f8a7105b9615bc8417b9d3
Marie-Therese Fischer <mt.fischer@gmx.at>
2016-12-02 19:10:01 +0100

    add basic html and css

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(express.static(__dirname + '/css'));
+
 app.get('/', (req, res) => {
-	res.status(200).send('Great info about herbs incoming!');
+	res.sendFile(path.join(__dirname, '/html/index.html'));
 });
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, `/html/${req.url}.html`));
+});
+
 
 app.listen(PORT, () => {
 	console.log(`App listening on port ${PORT}`);
